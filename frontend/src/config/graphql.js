c5635d28648faae7b5eacc942352db99606def17
Bertrand <bertrandmasabo22@gmail.com>
2023-05-25 00:52:26 +0200

    Added dotenv library and backend url env var

--- frontend/src/config/graphql.js
+++ frontend/src/config/graphql.js
@@ -20,7 +20,7 @@ const cache = new InMemoryCache({
 });
 
 export const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql",
+  uri: process.env.REACT_APP_BACKEND_URL,
   // cache: new InMemoryCache(),
   cache: cache,
 });
